551630accb08904f8931958b480a1b869c7e24dd
Aiden <tempestaiden@gmail.com>
2023-01-26 22:27:54 +0000

    help

--- ga.py
+++ ga.py
@@ -1,18 +1,49 @@
-# main file idk
+import json
 
-#generate initial population
 
-#fitness function
+class Solution(str):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-#selection
-
-#crossover
-
-#mutation
+    def calculate_fitness():
+        pass
+    
+    def crossover():
+        pass
+    
+    def mutation():
+        pass
+    
 
 def generate_timetable():
     print("Generating initial population")
-    generate_initial_population
+    generate_initial_population()
     
+def get_config_data():
+    print("Reading config file")
+    file = open("config.json", "r")
+    data = json.load(file)
+    teachers = data["teachers"]
+    rooms = data["rooms"]
+    student_groups = data["student_groups"]
+    modules = data["modules"]
+    time_slots = data["time_slots"]
+    file.close()
+    return teachers, rooms, student_groups, modules, time_slots
+    
+
 def generate_initial_population():
-    pass
+    teachers, rooms, student_groups, modules, time_slots = get_config_data()
+    teachers_id = []
+    for teacher in teachers:
+        teachers_id.append(teacher["id"])
+    rooms_id = []
+    for room in rooms:
+        rooms_id.append(room["id"])
+    student_groups_id = []
+    for student_group in student_groups:
+        student_groups_id.append(student_group["id"])
+    modules_id = []
+    for module in modules:
+        modules_id.append(module["id"])
+    
