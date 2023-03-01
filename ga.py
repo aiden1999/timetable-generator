5c41a360a063c7f01f040914c0859b8d5a1a8911
Aiden <tempestaiden@gmail.com>
2023-03-01 23:02:45 +0000

    functional binary search probably

--- ga.py
+++ ga.py
@@ -5,16 +5,13 @@ import random
 def generate_timetable():
     print("Generating initial population")
     population = generate_initial_population()
-    # create list to store fitnesses
+    population_fitness = []
     for solution in population:
         sol_fitness = calculate_fitness(solution)
-        print(sol_fitness)  # delete later
-    # pick two parents based on fitness
-    # normalise fitness
-    # choose random number between 0 and 1
-    # case switch normalised ranges for each solution
-    # remove chosen parent, recalculate for second parent
-    # selection can be its own function
+        population_fitness.append(sol_fitness)
+    # break condition if fitness = 0
+    print(population_fitness)  # delete later
+    parent_a, parent_b = select_parents(population_fitness)
 
 
 def get_config_data():
@@ -28,17 +25,15 @@ def get_config_data():
 def generate_initial_population():
     data = get_config_data()
 
-    sessions = []
+    sessions, rooms_id, time_slots_id = [], [], []
     for module in data["modules"]:
         session = [module["id"], module["student_group"], module["teachers"]]
         for i in range(int(module["hours"])):
             sessions.append(session)
 
-    rooms_id = []
     for room in data["rooms"]:
         rooms_id.append(room["id"])
 
-    time_slots_id = []
     day_number = 1
     for day in data["time_slots"]:
         for start_time in range(len(day["start_times"])):
@@ -47,7 +42,7 @@ def generate_initial_population():
         day_number += 1
 
     population = []
-    for i in range(10):
+    for i in range(10):  # could change so create more than 10?
         solution = create_complete_solution(sessions, rooms_id, time_slots_id)
         population.append(solution)
 
@@ -73,7 +68,7 @@ def create_complete_solution(sessions, rooms, time_slots):
 
 
 def calculate_fitness(solution):
-    solution.sort(key=lambda x: x[0])  # sort by time slot
+    solution.sort(key = lambda x: x[0])  # sort by time slot
     clash_count = 0
     for i in range(len(solution) - 1):
         if solution[i][0] != solution[i + 1][0]:  # no sessions happening at the same time
@@ -91,8 +86,45 @@ def calculate_fitness(solution):
     if clash_count == 0:
         fitness = 0
     else:
-        fitness = 1 / clash_count    
-    return fitness 
+        fitness = 1 / clash_count
+    return fitness
+
+
+def select_parents(population_fitness):
+    # normalise fitness
+    # choose random number between 0 and 1
+    # case switch normalised ranges for each solution
+    # remove chosen parent, recalculate for second parent
+    # selection can be its own function
+    fitness_total = 0
+    for value in population_fitness:
+        fitness_total += value
+        
+    range_upper_limits = []
+    for i in range(len(population_fitness)):
+        value_norm = population_fitness[i] / fitness_total
+        print(value_norm)  # delete later
+        range_upper_limits.append(value_norm)
+        if i != 0:
+            range_upper_limits[i] += range_upper_limits[i - 1]
+    range_upper_limits[len(population_fitness) - 1] = 1
+            
+    choice = random.random()
+    lower, upper = 0, len(population_fitness) - 1)
+    found_parent = False
+    while not found_parent:
+        middle = (lower + upper) // 2
+        if lower == upper - 1:
+            found_parent = True
+            parent_a = upper
+        elif population_fitness[middle] < choice:
+            lower = middle
+        else:
+            upper = middle
+    
+    
+                
+    return parent_a
 
 def crossover():
     pass  # delete later
