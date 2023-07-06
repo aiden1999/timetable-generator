377a429992a43c5450d9fccb7570a189361afdca
Aiden Tempest <tempestaiden@gmail.com>
2023-07-06 16:54:50 +0200

    help

--- testing.py
+++ testing.py
@@ -1,24 +1,29 @@
-import random
+class Session:
+    def __init__(self, time_slot, room, student_groups, module, teacher):
+        self.time_slot = time_slot
+        self.room = room
+        self.student_groups = student_groups
+        self.module = module
+        self.teacher = teacher
 
 
-def choose_parent(range_limits):
-    choice = random.random()
-    print("choice: ", choice)  # debugging
-    lower, upper = 0, len(range_limits) - 1
-    found_parent = False
-    while not found_parent:
-        middle = (lower + upper) // 2
-        print("lower: ", lower, " middle: ", middle, " upper: ", upper)  # debugging
-        if lower == upper - 1:
-            found_parent = True
-            parent_index = upper
-        elif range_limits[middle] < choice:
-            lower = middle
-        else:
-            upper = middle
-    return parent_index
+class Timetable(list[Session]):
+    def __init__(self):
+        super().__init__()
 
 
-limits = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-index = choose_parent(limits)
-print("index " + str(index))
+session1 = Session("time1", "room1", "sg1", "mod1", "teacher1")
+session2 = Session("time1", "room1", "sg1", "mod1", "teacher1")
+session2.time_slot = "time2"
+session2.room = "room2"
+session2.student_groups = "sg2"
+session2.module = "mod2"
+session2.teacher = "teacher2"
+
+timetable = Timetable()
+
+timetable.append(session1)
+timetable.append(session2)
+
+for session in timetable:
+    print(session)
